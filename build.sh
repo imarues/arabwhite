9e8a18eb126e576353fb175f0761e49d9de4d17f
imarues <rhom.adel1212@gmail.com>
2026-09-08 20:39:21 +0300

    Compile two-finger hold language gesture

--- build.sh
+++ build.sh
@@ -19,7 +19,6 @@ CLANG="$(xcrun --sdk iphoneos --find clang)"
   -miphoneos-version-min=15.0 \
   -dynamiclib \
   -fobjc-arc \
-  -fblocks \
   -fmodules \
   -O2 \
   -Wall \
@@ -33,7 +32,9 @@ CLANG="$(xcrun --sdk iphoneos --find clang)"
   "$ROOT/Sources/Tweak.m" \
   "$ROOT/Sources/WGTranslations.m" \
   "$ROOT/Sources/WGLanguageOverlay.m" \
+  "$ROOT/Sources/WGTwoFingerLanguageGesture.m" \
   "$ROOT/Sources/WGWindowLanguageGestures.m" \
+  "$ROOT/Sources/WGTwoFingerHoldGesture.m" \
   "$ROOT/Sources/WGRootTextureFixCompile.m" \
   "$ROOT/Sources/WGOfflineLanguageFix.m" \
   -I"$ROOT/Sources" \
